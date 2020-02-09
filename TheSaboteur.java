ba728b1a7b292467ec82566ad89e40df053c74a0
Jason Copp <jason.copp@initlive.com>
2020-02-09 12:58:36 -0500

    no message

--- TheSaboteur.java
+++ TheSaboteur.java
@@ -19,20 +19,19 @@ public class TheSaboteur extends AdvancedRobot
 	 */
 	
 	final private int weaveWidth = 40;
-	final private double bulletStrength = 3;
-	final private boolean shouldMove = true;
+	final private double bulletStrength = 0.1;
+	final private boolean shouldMove = false;
 	
 	private double distanceToTarget = 0;
 	private double bearingToTarget = 0;
 	private double moveDirection = 1;
 	private boolean activeRadar = false;
 	private boolean firstScan = true;
-	private double previous = 0;
-	private double previousBearingToTarget = 0;
-	private double previousDistance = 0;
-	private String target = "";
+//	private double previousBearingToTarget = 0;
+//	private double previousDistance = 0;
+	private String scannedTarget = "";
 	private double closestTargetDistance = 100000;
-	private String closestTarget = "";
+//	private String closestTarget = "";
 	private int numberOfTanks = 0;
 	private String tankToTarget = "";
 	private boolean tickSinceLastScan = false;
@@ -123,25 +122,28 @@ public class TheSaboteur extends AdvancedRobot
 	 */
 	
 	public void onScannedRobot(ScannedRobotEvent e) {
-		if (target != e.getName()) {
-			//out.printf("Scanned: %s\n", e.getName());
-		}
-		target = e.getName();
+//		if (currentTarget != e.getName()) {
+//			//out.printf("Scanned: %s\n", e.getName());
+//		}
+		scannedTarget = e.getName();
+		
 		if (firstScan) {
-			out.printf("Enemy: %s Distance: %f\n",target, e.getDistance());
+			// this is run on the first 360 degree sweep, used to find the closest target
+			out.printf("Enemy: %s Distance: %f\n",scannedTarget, e.getDistance());
 			if (e.getDistance() < closestTargetDistance)
 			{
-				tankToTarget = target;
+				tankToTarget = scannedTarget;
 				closestTargetDistance = e.getDistance();
 			}
 			numberOfTanks++;
 			return;
 		}
-		if (tankToTarget != "" && target != tankToTarget) {
+		if (tankToTarget != "" && scannedTarget != tankToTarget) {
+			// there is a target we wish to target. if this isn't the currently scannedTarget, sweep for it once
 			out.printf("Re-acquiring for target: %s\n", tankToTarget);
 			if (!activeRadar) {
 				setTurnRadarRight(10360);
-				activeRadar = true;
+				activeRadar = true; //prevent the main() from resetting the sweep
 				return;
 			}
 			if (this.getRadarTurnRemaining() > 10000) {
@@ -151,8 +153,12 @@ public class TheSaboteur extends AdvancedRobot
 		}
 		activeRadar = false;
 		if (!tickSinceLastScan) {
+			// there has been not been a tick of the main while() loop since the last scan, then we're likely
+			// looping through more targets in the radar's view, even though we already
+			// have target lock on the one we want. so, abort this scan and continue focusing on the current target
 			return;
 		}
+		
 		tankToTarget = "";
 		double gunBearing = getGunHeading();
 		double heading = getHeading();
@@ -175,28 +181,32 @@ public class TheSaboteur extends AdvancedRobot
 //		Output : 6.04339
 //		c = sqrt(a^2 + b^2 - 2*a*b*cos(C))
 //		calculate the distance the bullet has to travel (side c)
-		double a = distanceToTarget;
-		double bestGuess = distanceToTarget;
-		double B = 0;
-		boolean reversed = false;
+		double bestGuessCalculatedDistanceToFutureTarget = distanceToTarget;
+		double leadFiringAngle = 0;
+//		boolean reversed = false;
+//		Calculate a triangle based on the target's angle and velocity, bulletspeed, etc, to determine its likely location
+//		The "long" side of the triangle won't account for how much longer (or shorter) the time the bullet will take to arrive
+//		I'm sure there is smart math to anticipate this, but i found if I loop the calculation with my best guess, after
+//		five loops the margin of error is well below 1 pixel.
 		for (int i=0;i<5;i++) {
-			double b = bestGuess/bulletSpeed*targetVelocity;
-			double C = 180 - (360 - absoluteTargetBearing) - targetHeading;
+			double proposedDistanceToTarget = bestGuessCalculatedDistanceToFutureTarget/bulletSpeed*targetVelocity;
+			double targetAngle = 180 - (360 - absoluteTargetBearing) - targetHeading;
 //			if (C > 180) {
 //				C -= 180;
 //			}
 //			else if (C < -180) {
 //				C += 180;
 //			}
-			C = Utils.normalRelativeAngleDegrees(C);
-	//		c2=a2+b2−2abcosγ
-			double c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2) - (2*a*b*Math.cos(C*Math.PI/180)));
-			// next find the B angle
-			//cos B = (a2 + c2 − b2)/2ac
-	
-			B = Math.acos((Math.pow(a, 2) + Math.pow(c,2) - Math.pow(b,2))/(2*a*c))*180/Math.PI;
-			if (C < 0) B = B * -1;
-			bestGuess = c;
+			targetAngle = Utils.normalRelativeAngleDegrees(targetAngle);
+//			c2=a2+b2−2abcosγ - determine length of 3rd side of triangle using SAS
+//			remember cos and acos use RADIANS, so convert degrees to radians by degrees*pi/180
+			double calculatedDistanceToFutureTarget = Math.sqrt(Math.pow(distanceToTarget,2) + Math.pow(proposedDistanceToTarget,2) - (2*distanceToTarget*proposedDistanceToTarget*Math.cos(targetAngle*Math.PI/180)));
+//			next find the B angle using SS
+//			cos B = (a2 + c2 − b2)/2ac
+//			remember cos and acos use RADIANS, so convert acos BACK to degrees with radians/pi*180	
+			leadFiringAngle = Math.acos((Math.pow(distanceToTarget, 2) + Math.pow(calculatedDistanceToFutureTarget,2) - Math.pow(proposedDistanceToTarget,2))/(2*distanceToTarget*calculatedDistanceToFutureTarget))*180/Math.PI;
+			if (targetAngle < 0) leadFiringAngle = leadFiringAngle * -1; 
+			bestGuessCalculatedDistanceToFutureTarget = calculatedDistanceToFutureTarget;
 		}
 		
 		//TODO: determine if calculated trajectory would be outside the bounds of the playing field 
@@ -223,7 +233,7 @@ public class TheSaboteur extends AdvancedRobot
 //		double distance_offset = ((cc+previous)/2) * relational_difference;
 //		out.printf(" distance_offset: %f",distance_offset);
 //		out.printf(" unknown: %f",this.getGunTurnRemaining());
-		double turnGun = Utils.normalRelativeAngleDegrees(bearingToTarget-gunBearing+heading+B);
+		double turnGun = Utils.normalRelativeAngleDegrees(bearingToTarget-gunBearing+heading+leadFiringAngle);
 
 		
 		//		double delta = bearingDelta.average();
@@ -243,8 +253,8 @@ public class TheSaboteur extends AdvancedRobot
 		
 		
 //		previous = cc;
-		previousDistance = distanceToTarget;
-		previousBearingToTarget = (heading+bearingToTarget);
+//		previousDistance = distanceToTarget;
+//		previousBearingToTarget = (heading+bearingToTarget);
 		tickSinceLastScan = false;
 	}
 
